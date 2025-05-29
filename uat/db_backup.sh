d724d4d935c4218f1063ac0e631c59e17df644a9
PhoneMT <mr.phoneminthu@gmail.com>
2025-05-29 13:53:50 +0630

    Uat (#3)
    * build(env): add env file to server
    
    * build(env): add env files to server
    
    * fix(backup_script): fix script

--- uat/db_backup.sh
+++ uat/db_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Load environment variables from the .env file
-export $(grep -v '^#' .env.uat | xargs)
+export $(grep -v '^#' .env.be | xargs)
 
 # Set up timestamp and backup file name
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
